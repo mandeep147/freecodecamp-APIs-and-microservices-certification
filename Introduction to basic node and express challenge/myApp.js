b6a05b788140bb7f8969968ea647f4c2998bb569
mandeep147 <mndpkaur14@gmail.com>
2019-01-19 20:20:28 -0600

    read query params

--- Introduction to basic node and express challenge/myApp.js
+++ Introduction to basic node and express challenge/myApp.js
@@ -64,7 +64,11 @@ app.get('/:word/echo', function(req, res){
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.get("/name", function(req, res){
+    let firstname = req.query.first
+    let lastname = req.query.last
+    res.json({name: firstname + " " +lastname})
+})
 
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
