db144d79a5cb180c38a6353a52b5cab219be9e26
mandeep147 <mndpkaur14@gmail.com>
2019-01-20 23:27:53 -0600

    find and update a record

--- Introduction to the MongoDB/myApp.js
+++ Introduction to the MongoDB/myApp.js
@@ -195,7 +195,14 @@ var findPersonById = function(personId, done) {
 var findEditThenSave = function(personId, done) {
     var foodToAdd = 'hamburger';
 
-    done(null/*, data*/);
+    Person.findById(personId, function(err, data) {
+        if (err) {
+            done(err);
+        }
+
+        data.favoriteFoods.push(foodToAdd);
+        data.save((err, data) => (err ? done(err) : done(null, data)));
+    });
 };
 
 /** 9) New Update : Use `findOneAndUpdate()` */
